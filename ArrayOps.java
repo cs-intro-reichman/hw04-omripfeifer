7cafc10d9f33d8cdca816afd7826120217ae072d
omripfeifer <omri.pfeifer@post.runi.ac.il>
2024-01-09 16:27:10 +0200

    changes

--- ArrayOps.java
+++ ArrayOps.java
@@ -4,18 +4,41 @@ public class ArrayOps {
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        
+        int sumFullArray = 0 ;
+        int sumArray = 0 ;  
+        for (  int i = 0 ; i < array.length; i ++ ){
+            sumArray += array[i];
+        }
+        for ( int i = 1; i <= array.length; i++ ){
+            sumFullArray += i ;
+        }
+        int missInteg = sumFullArray - sumArray ; 
+        return missInteg;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int maxValue = Integer.MIN_VALUE ; 
+        int secondMaxValue = Integer.MIN_VALUE ; 
+        for ( int i = 0 ; i <  length.array ; i++ ){
+            if ( array[i] > maxValue ){
+                 secondMaxValue = maxValue ;
+                 maxValue = array[1] ; 
+            } else if ( array[i] > secondMaxValue && array[i] < maxValue ){
+                secondMaxValue = array[i];
+            }
+        }
+        return secondMaxValue ;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+      boolean check = false 
+       for ( int i = 0 ; i < array1.length ; i++ ){
+        for ( int j = 0 ; j < array2.length ; j++){
+            if ( array1[i] == array2[j])
+        }
+       }
+        return true ;
     }
 
     public static boolean isSorted(int [] array) {
