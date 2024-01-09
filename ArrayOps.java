a67fd276dae5ef1920649a9786041c2cae5adf70
omripfeifer <omri.pfeifer@post.runi.ac.il>
2024-01-09 16:47:25 +0200

    change

--- ArrayOps.java
+++ ArrayOps.java
@@ -40,13 +40,16 @@ public class ArrayOps {
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
       boolean check = false ;
+
        for ( int i = 0 ; i < array1.length ; i++ ){
         for ( int j = 0 ; j < array2.length ; j++){
             if ( array1[i] == array2[j]){
                 check = true ;
             }
         }
+        check = false ;
        } 
+
        for ( int i = 0 ; i < array2.length ; i++ ){
         for ( int j = 0 ; j < array1.length ; j++){
             if ( array1[i] == array2[j]){
